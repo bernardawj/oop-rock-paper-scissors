ad382078f511b3bce5b75cd669b558eeae35b3f7
Bernard Ang <work@bernardawj.com>
2024-03-29 21:24:14 +0800

    chore: add base project skeleton

--- Program.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RockPaperScissors;
-
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World");
-    }
-}
---
+++ RockPaperScissors.Application/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace RockPaperScissors.Application;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
